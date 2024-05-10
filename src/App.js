76e21cfbc575310aa93d6e0268b188a665f4a399
BoXi9252 <zhaoyanglove2022@126.com>
2024-05-10 18:59:28 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+import StarRating from "./components/StarRating";
+import Dialog from "./components/Dialog";
+import { Button } from "antd";
+
+function App() {
+  const [isShowDialog, setIsShowDialog] = React.useState(false);
+  return (
+    <div className="App">
+      <div className="container">
+        <StarRating />
+        <Dialog
+          visible={isShowDialog}
+          onCancel={() => {
+            setIsShowDialog(false);
+          }}
+        />
+        <div className="button-wrapper">
+          <Button
+            type="primary"
+            onClick={() => {
+              setIsShowDialog(true);
+            }}
+          >
+            open dialog
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,38 @@
+const Constants = {
+  /* info of star */
+  starInfo: [
+    {
+      idx: 1,
+      text: "Fine",
+    },
+    {
+      idx: 2,
+      text: "Acceptable",
+    },
+    {
+      idx: 3,
+      text: "Decent",
+    },
+    {
+      idx: 4,
+      text: "Good",
+    },
+    {
+      idx: 5,
+      text: "Great",
+    },
+  ],
+  /** 未评分的星星信息 */
+  defStar: {
+    idx: 0,
+    text: "Unknown"
+  },
+  /** 星星的大小 */
+  starSize: 30,
+  /** 激活的星颜色 */
+  actColor: "#ff6",
+  /** 未激活的星颜色 */
+  defColor: "#fff",
+};
+
+export default Constants;
